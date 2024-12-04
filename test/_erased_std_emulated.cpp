4c12d3ce6db5522478444068487e6361899ab9c8
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2024-12-04 16:25:00 +0100

    reactivate code

--- test/_erased_std_emulated.cpp
+++ test/_erased_std_emulated.cpp
@@ -75,10 +75,10 @@ TEST_CASE("std emulated function") {
                   move_only_function<std::string(const std::string)>,
                   move_only_function<std::string(const std::string)>>);
     move_only_function<std::string(const std::string)> f2 { std::move(f) };
-   // REQUIRE(!has_data(get_virtual_void(f)));
-    //REQUIRE(f2(", bye") == "hello world");
-    //REQUIRE(unerase_cast<functor_t>(get_virtual_void(f2))->s_ ==
-    //        "hello world, bye");
+    REQUIRE(!has_data(get_virtual_void(f)));
+    REQUIRE(f2(", bye") == "hello world");
+    REQUIRE(unerase_cast<functor_t>(get_virtual_void(f2))->s_ ==
+            "hello world, bye");
   }
 }
 }  // namespace
